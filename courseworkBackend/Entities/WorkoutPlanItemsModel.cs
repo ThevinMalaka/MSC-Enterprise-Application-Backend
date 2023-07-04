7944731b0da9ab36a5b411432159d726132f4a55
Thevin Malaka <thevin@orelit.com>
2023-07-04 23:37:03 +0530

    data feed part

---
+++ courseworkBackend/Entities/WorkoutPlanItemsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace courseworkBackend.Entities
+{
+	public class WorkoutPlanItemsModel
+	{
+		 public int Id { get; set; }
+
+		//  how to get foreign key from WorkoutPlanModel
+		//  public int WorkoutPlanId { get; set; }
+		
+
+	}
+}
+
